f81427322c15a02982bd8e643e51f3b6eb6926f7
faustinegl <faustine.galichet@edu.ece.fr>
2021-10-01 10:20:13 +0200

    q4

--- js/API_Weather.js
+++ js/API_Weather.js
@@ -11,8 +11,10 @@ const API_URL_ICON = "http://openweathermap.org/img/wn/";
 class API_WEATHER{
   constructor(city){
     // Si la ville n'est pas définit alors la ville par défault est Paris
-    if(city === undefined){
+    city=document.getElementById("city-input").value;
+    if(city === ""){
       city = "paris";
+      
     }
     this.city = city;
   }
